05258486b879bb16599ec5af7e1ce13c3431c8b6
deepakthapa1423@gmail.com <deepakthapa1423@gmail.com>
2023-11-18 20:16:06 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+
+import React from "react";
+
+class App extends React.Component {
+  render() {
+    const { name } = this.props;
+    return (
+      <>
+        <h1>
+          Hello world
+        </h1>
+      </>
+    );
+  }
+}
+
+export default App;
+
